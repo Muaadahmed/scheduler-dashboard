19dce94aa47a96ca380b6bf096d21ccf31c4782e
Muaadahmed <muaad.abdi.ahmed@gmail.com>
2021-12-28 15:49:58 -0500

    Create Panel component and import in Dashboard component; 4 panels displayed intially, each panel takes up viewport on click

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -4,6 +4,9 @@ import classnames from "classnames";
 
 import Loading from "./Loading";
 
+import Panel from "./Panel";
+import { set } from "lodash";
+
 const data = [
   {
     id: 1,
@@ -29,16 +32,33 @@ const data = [
 
 class Dashboard extends Component {
   state = {
-    loading: false
+    loading: false,
+    focused: null
   };
+
+  selectPanel (id){
+    this.setState({
+      focused: id
+    });
+  }
   render() {
-    const dashboardClasses = classnames("dashboard");
+    const dashboardClasses = classnames("dashboard", {
+      "dashboard--focused": this.state.focused
+    });
+
+    const panels = data.filter(panel => this.state.focused === null || this.state.focused === panel.id).map(panel => {
+      return <Panel key={panel.id} id={panel.id} label={panel.label} value={panel.value} onSelect={(event) => this.selectPanel(panel.id)}/>
+    });
 
     if (this.state.loading) {
       return <Loading/>;
     }
 
-    return <main className={dashboardClasses} />;
+    return (
+      <main className={dashboardClasses}>
+        {panels}
+      </main>
+    );
   }
 }
 
